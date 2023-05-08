270d88628aef62c0d2f3dae1fc6245cb691374b5
Saleamlack <saleamlack7@gmail.com>
2023-05-08 18:20:36 +0300

    Start the game

---
+++ main.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'lib/color'
+require_relative 'lib/config_files'
+require_relative 'lib/display'
+require_relative 'lib/game'
+
+Game.new.start_game
